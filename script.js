f9c1c5d85329497180ccf96f11cd5633fe10806d
Welyson Filipe <welysonfilipe@gmail.com>
2025-07-12 18:52:26 -0300

    Exclusão da chave da API do Gemini do código.

--- script.js
+++ script.js
@@ -5,7 +5,35 @@ const askButton = document.getElementById('askButton');
 const aiResponse = document.getElementById('aiResponse');
 const form = document.getElementById('form');
 
-const enviarFormulario = (event) => {
+const perguntarAI = async (question, game, apiKey) => {
+    const model = "gemini-2.5-flash"
+    const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
+    const pergunta = `
+        olha, tenho esse jogo ${game} e queria saber ${question}
+    `
+
+    const contents = [{
+        parts: [{
+            text: pergunta
+        }]
+    }]
+
+    // Chamada API
+    const response = await fetch(geminiURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'aplication/json'
+        },
+        body: JSON.stringify({
+            contents
+        })
+    })
+
+    const data = await response.json()
+    return data.candidates[0].content.parts[0].text
+}
+
+const enviarFormulario = async (event) => {
     event.preventDefault()
     console.log(event)
 }
